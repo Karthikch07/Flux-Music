475a45b1ce2672e747e703233d6cbec49785c59f
CH.Karthik <chkarthik618@gmail.com>
2025-09-22 11:38:38 +0530

    Initial commit: Flux Music React app with Tailwind CSS and music player functionality

---
+++ src/components/Display.jsx
@@ -0,0 +1,34 @@
+import React, { useEffect, useRef } from 'react'
+import { Route, Routes, useLocation } from 'react-router-dom'
+import DisplayHome from './Displayhome'
+import DisplayAlbum from './DisplayAlbum'
+import { albumsData } from '../assets/assets'
+
+const Display = () => {
+
+  const displayRef = useRef();
+  const location = useLocation();
+  const isAlbum = location.pathname.includes('album');
+  const albumId = isAlbum ? location.pathname.slice(-1) : "";
+  const albumData = albumsData[Number(albumId)];
+  const bgColor = albumData?.bgColor || '#121212';
+
+  useEffect(() => {
+    if (isAlbum && albumData) {
+      displayRef.current.style.background = `linear-gradient(${bgColor}, #121212)`;
+    } else {
+      displayRef.current.style.background = '#121212';
+    }
+  }, [isAlbum, bgColor, albumData])
+
+  return (
+    <div ref={displayRef} className='w-[100%] m-2 px-6 pt-4 rounded bg-[#121212] text-white overflow-auto lg:w-[75%] lg:ml-0'>
+        <Routes>
+             <Route path='/' element={<DisplayHome />} />
+             <Route path='/album/:id' element={<DisplayAlbum />} />
+        </Routes>
+    </div>
+  )
+}
+
+export default Display
